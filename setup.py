aa90f06a94b7fee9241c9d7f08576a08eb4eae9f
Matt Shannon <matt.shannon@cantab.net>
2017-06-13 08:18:28 +0000

    prepare to release 0.4
    Ignore-this: 27f47367820138002e2f1df4a3086b0c
    
    darcs-hash:20170613081828-3a4db-3e8173576d8d613b2ca5037e949e8462302178a0

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ else:
 
 setup(
     name='mcd',
-    version='0.4.dev1',
+    version='0.4',
     description='Mel cepstral distortion (MCD) computations in python.',
     url='http://github.com/MattShannon/mcd',
     author='Matt Shannon',
